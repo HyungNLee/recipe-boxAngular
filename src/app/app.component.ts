e82806a5ccb66d5356f369e4fe3d068529201c2d
Hyung Lee and Regina Nurieva <regina@>
2018-10-23 11:11:16 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  padThaiIncredients: string[] = [
+    'Ketchup',
+    'More Ketchup',
+    'Noodles',
+    'Even More Ketchup'
+  ];
+  padThaiDirections: string[] = [
+    'Take ketchup and heat it up.',
+    'Go to McDonalds and steal some ketchup packs.',
+    'Add noodles and heat with ketchup.'
+  ];
+
+  pastaSaladIncredients: string[] = [
+    'Pasta',
+    'Mayo',
+    'Salt',
+    'Pepper'
+  ];
+  pastaSaladDirections: string[] = [
+    'Take pasta and boil it.',
+    'MIx it with mayo.',
+    'Add salt and pepper to taste.'
+  ];
+
+  pirozhkyIncredients: string[] = [
+    'Russian',
+    'Kitchen',
+    'Oven',
+    'Dough'
+  ];
+  pirozhkyDirections: string[] = [
+    'Take russian.',
+    'Place it in the kitchen.',
+    'Ask for pirozhky.'
+  ];
+
+  recipeList: Recipe[] = [
+    new Recipe('Pad Thai', this.padThaiIncredients, this.padThaiDirections),
+    new Recipe('Pasta Salad', this.pastaSaladIncredients, this.pastaSaladDirections),
+    new Recipe('Pirozhky', this.pirozhkyIncredients, this.pirozhkyDirections)
+  ];
+
+  
+
+}
